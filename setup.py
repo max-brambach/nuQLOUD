b5c5cfb3ce77dc47de91fad5ea99553a30533639
Max Brambach <30292625+max-brambach@users.noreply.github.com>
2024-05-23 16:06:51 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='AnalysisOfLocalNeighbourhoods',
-      version='0.2',
-      description='some description',
+      version='0.3',
+      description='Software for the generation of organisational features from point clouds.',
       url='no_url',
       author='Max Brambach',
-      author_email='max.brambach@uzh.ch',
-      license='none yet',
+      author_email='max.brambach@gmail.com',
+      license='MIT',
       packages=['ALoNe'],
       zip_safe=False)
